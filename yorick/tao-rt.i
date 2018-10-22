95c9e9f78d15cac3a0745630c7a3183c06cc2e75
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2018-10-22 18:36:31 +0200

    Add many functions to reflect most of TAO library capabilities

--- yorick/tao-rt.i
+++ yorick/tao-rt.i
@@ -14,7 +14,6 @@
 
 if (is_func(plug_in)) plug_in, "tao_rt";
 
-extern tao_test;
 extern tao_get_current_time;
 extern tao_get_monotonic_time;
 /* DOCUMENT t = tao_get_current_time();
@@ -48,19 +47,137 @@ extern tao_create_shared_object;
    SEE ALSO: tao_attach_shared_object.
  */
 
+local TAO_INT8, TAO_UINT8, TAO_INT16, TAO_UINT16, TAO_INT32, TAO_UINT32,
+    TAO_INT64, TAO_UINT64, TAO_FLOAT32, TAO_FLOAT64;
+extern tao_attach_shared_array;
+extern tao_attach_shared_camera;
 extern tao_attach_shared_object;
 /* DOCUMENT obj = tao_attach_shared_object(ident);
          or obj = tao_attach_shared_object(ident, type);
+         or arr = tao_attach_shared_array(ident);
+         or cam = tao_attach_shared_camera(ident);
 
-     Attachs the shared TAO object identified by `ident` to the address space
-     of the Yorick process and returns it.  The returned shared object `obj`
-     is automatically detached when no longer referenced by Yorick.
+     Function `tao_attach_shared_object` attaches the shared TAO object
+     identified by `ident` to the address space of the Yorick process and
+     returns it.  The returned shared object `obj` is automatically detached
+     when no longer referenced by Yorick.  Optional argument `type` can be one
+     of `TAO_SHARED_OBJECT`, `TAO_SHARED_ARRAY` or `TAO_SHARED_CAMERA` if a
+     specific object type is expected (`TAO_SHARED_OBJECT` is the default).
 
-     Optional argument `type` can be one of `TAO_SHARED_OBJECT`,
-     `TAO_SHARED_ARRAY` or `TAO_SHARED_CAMERA` if a specific object type is
-     expected (`TAO_SHARED_OBJECT` is the default).
+     Function `tao_attach_shared_array` attaches the shared TAO array
+     identified by `ident` to the address space of the Yorick process and
+     returns it.  The returned shared object `arr` is automatically detached
+     when no longer referenced by Yorick.
 
-   SEE ALSO: tao_create_shared_object.
+     Function `tao_attach_shared_camera` attaches the shared TAO camera
+     identified by `ident` to the address space of the Yorick process and
+     returns it.  The returned shared object `cam` is automatically detached
+     when no longer referenced by Yorick.
+
+     The returned results have the following members:
+
+         obj.ident      identifier of shared object;
+         obj.type       type of shared object;
+         obj.size       size (in bytes) of shared object;
+
+         arr.ident      identifier of shared array;
+         arr.type       type of shared array (that is TAO_SHARED_ARRAY);
+         arr.size       size (in bytes) of shared array;
+         arr.eltype     type of elements of shared array;
+         arr.ndims      number of dimensions of shared array;
+         arr.dims       dimensions (as with `dimsof`) of shared array;
+         arr.data       data (as a Yorick array) of shared array;
+         arr.counter    counter of shared array;
+         arr.timestamp  time-stamp of shared array (a pair of integers,
+                        `[s,ns]`, with the number of seconds and nanoseconds);
+
+         cam.ident      identifier of shared array;
+         cam.type       type of shared array (that is TAO_SHARED_ARRAY);
+         cam.size       size (in bytes) of shared array;
+         cam.state      camera state (0 if device not yet open, 1 if device
+                        open but no acquisition is running, 2 if acquisition
+                        is running);
+         cam.bias       detector bias;
+         cam.gain       detector gain;
+         cam.gamma      gamma correction factor;
+         cam.fullwidth  detector horizontal size (in pixels);
+         cam.fullheight detector vertical size (in pixels);
+         cam.depth      bits per pixel in the raw captured images;
+         cam.eltype     type of elements of processed images;
+         cam.xoff       horizontal offset of ROI (in pixels);
+         cam.yoff       vertical offset of ROI (in pixels);
+         cam.width      number of macro-pixels per line in acquired images;
+         cam.height     number of lines of macro-pixels in acquired images;
+         cam.roi        region of interest: `[xoff,yoff,width,height]`;
+         cam.rate       number of frames per second;
+         cam.exposure   exposure duration (in seconds);
+
+     The possible values of `eltype` are:
+
+         -----------------------------------------
+         Constant   Value  Description
+         -----------------------------------------
+         TAO_INT8      1   Signed 8-bit integer
+         TAO_UINT8     2   Unsigned 8-bit integer
+         TAO_INT16     3   Signed 16-bit integer
+         TAO_UINT16    4   Unsigned 16-bit integer
+         TAO_INT32     5   Signed 32-bit integer
+         TAO_UINT32    6   Unsigned 32-bit integer
+         TAO_INT64     7   Signed 64-bit integer
+         TAO_UINT64    8   Unsigned 64-bit integer
+         TAO_FLOAT32   9   32-bit floating-point
+         TAO_FLOAT64  10   64-bit floating-point
+         -----------------------------------------
+
+
+   SEE ALSO: tao_create_shared_object, tao_create_shared_array,
+             tao_get_data, tao_wait_image.
+ */
+
+extern tao_get_data;
+/* DOCUMENT arr = tao_get_data(obj);
+         or tao_set_data, obj, arr;
+
+     The function `tao_get_data` yields the contents of a TAO shared array
+     `obj` as a Yorick array.
+
+   SEE ALSO: tao_create_shared_array, tao_attach_shared_object.
+ */
+
+extern tao_get_counter;
+extern tao_get_timestamp;
+/* DOCUMENT cnt = tao_get_counter(arr);
+         or tms = tao_set_timestamp(arr);
+
+     The function `tao_get_counter` yields the value of the image counter
+     of TAO shared array `arr`.
+
+     The function `tao_get_timestamp` yields the value of the time-stamp of
+     TAO shared array `arr`.  The returned time-stamp is a pair of two
+     integers, the number of seconds and the number of nanoseconds elapsed
+     since some unspecified epoch.
+
+     These two functions are respectively equivalent to:
+
+         arr.counter
+         arr.timestamp
+
+
+   SEE ALSO: tao_wait_image, tao_get_data, tao_get_monotonic_time.
+ */
+
+extern tao_wait_image;
+/* DOCUMENT arr = tao_wait_image(cam);
+         or arr = tao_wait_image(cam, secs);
+
+     The function `tao_wait_image` waits for a new image to be available from
+     TAO shared camera `cam`.  Optional argument `secs` is the maximum amount
+     of time to wait (in seconds).  If no new image is acquired during the
+     allowed time, an empty result is returned, otherwise a TAO shared array
+     is returned with the image data.  If `secs` is unspecified (not
+     recommended), the call blocks until a new image becomes available.
+
+   SEE ALSO: tao_create_shared_array, tao_attach_shared_object.
  */
 
 extern tao_lock;
@@ -71,7 +188,7 @@ extern tao_unlock;
          or tao_unlock, obj;
 
      The function `tao_lock` locks object `obj`.  If the object is locked by
-     another process, the call will block * until the object is unlocked.
+     another process, the call will block until the object is unlocked.
 
      The function `tao_try_lock` attempts to lock object `obj` without
      blocking.  If the object is locked by another process, `0` is returned;
@@ -85,4 +202,11 @@ extern tao_unlock;
  */
 
 extern _tao_init;
+/* DOCUMENT _tao_init;
+
+      Initializes constants of TAO plugin.  Can be called again to restore
+      them.
+
+   SEE ALSO: tao_attach_shared_object.
+ */
 _tao_init;
--- yorick/yor-tao-rt.c
+++ yorick/yor-tao-rt.c
@@ -36,8 +36,8 @@
 
 PLUG_API void y_error(const char *) __attribute__ ((noreturn));
 
-static void push_string(const char* str);
-
+static int tao_to_yorick_types[TAO_FLOAT64 - TAO_INT8 + 1];
+static int yorick_to_tao_types[Y_DOUBLE - Y_CHAR + 1];
 static char* buffer = NULL;
 static size_t bufsiz = 0;
 
@@ -87,6 +87,82 @@ report_errors(tao_error_t** errs)
     y_error(buffer);
 }
 
+static void push_array_data(tao_shared_array_t* arr)
+{
+    long dims[Y_DIMSIZE];
+    int d, ndims, type;
+    const void* src;
+    void* dst;
+    long nelem, elsize;
+
+    type = tao_get_shared_array_eltype(arr);
+    if (type >= TAO_INT8 && type <= TAO_FLOAT64) {
+        type = tao_to_yorick_types[type - TAO_INT8];
+    } else {
+        type = -1;
+    }
+    ndims = tao_get_shared_array_ndims(arr);
+    if (ndims < 0) {
+        y_error("invalid number of dimensions");
+    }
+    if (ndims >= Y_DIMSIZE) {
+        y_error("too many dimensions");
+    }
+    dims[0] = ndims;
+    nelem = 1;
+    for (d = 1; d <= ndims; ++d) {
+        dims[d] = tao_get_shared_array_size(arr, d);
+        nelem *= dims[d];
+    }
+    switch (type) {
+    case Y_CHAR:
+        elsize = sizeof(char);
+        dst = ypush_c(dims);
+        break;
+    case Y_SHORT:
+        elsize = sizeof(short);
+        dst = ypush_s(dims);
+        break;
+    case Y_INT:
+        elsize = sizeof(int);
+        dst = ypush_i(dims);
+        break;
+    case Y_LONG:
+        elsize = sizeof(long);
+        dst = ypush_l(dims);
+        break;
+    case Y_FLOAT:
+        elsize = sizeof(float);
+        dst = ypush_f(dims);
+        break;
+    case Y_DOUBLE:
+        elsize = sizeof(double);
+        dst = ypush_d(dims);
+        break;
+    default:
+        y_error("unsupported array type");
+        elsize = 0;
+        dst = NULL;
+    }
+    src = tao_get_shared_array_data(arr);
+    if (elsize > 0 && nelem > 0) {
+        memcpy(dst, src, nelem*elsize);
+    }
+}
+
+static void push_array_timestamp(tao_shared_array_t* arr)
+{
+    int64_t ts_sec, ts_nsec;
+    long dims[2];
+    long* ans;
+    tao_get_shared_array_timestamp(arr, &ts_sec, &ts_nsec);
+    dims[0] = 1;
+    dims[1] = 2;
+    ans = ypush_l(dims);
+    ans[0] = ts_sec;
+    ans[1] = ts_nsec;
+}
+
 /*---------------------------------------------------------------------------*/
 /* SHARED OBJECTS */
 
@@ -111,11 +187,23 @@ print_shared_object(void* addr)
         resize_buffer(200);
     }
     if (tao_lock_shared_object(&errs, obj) == 0) {
+        char small_buf[64];
+        char* type_name;
         int ident = tao_get_shared_object_ident(obj);
         int type = tao_get_shared_object_type(obj);
         size_t size = tao_get_shared_object_size(obj);
-        sprintf(buffer, "TAO shared object (ident=%d, type=%d, size=%ld)",
-                ident, type, (long)size);
+        if (type == TAO_SHARED_ARRAY) {
+            type_name = "TAO_SHARED_ARRAY";
+        } else if (type == TAO_SHARED_CAMERA) {
+            type_name = "TAO_SHARED_CAMERA";
+        } else if (type == TAO_SHARED_OBJECT) {
+            type_name = "TAO_SHARED_OBJECT";
+        } else {
+            sprintf(small_buf, "0x%08x", (unsigned int)type);
+            type_name = small_buf;
+        }
+        sprintf(buffer, "TAO shared object (ident=%d, type=%s, size=%ld)",
+                ident, type_name, (long)size);
         tao_unlock_shared_object(&errs, obj);
     } else {
         buffer[0] = '\0';
@@ -147,17 +235,114 @@ extract_shared_object(void* addr, char* name)
 {
     /* There is no needs to lock a shared object if we already have a reference
        on it (which is warranted if it is attached to our address space) and if
-       we only read immutable information. */
+       we only read immutable information.  Using string comparison is fast
+       enough (worst case here takes 75 ns, best case takes 58 ns). */
     tao_shared_object_t* obj = *(tao_shared_object_t**)addr;
-    if (strcmp(name, "ident") == 0) {
+    int c = name[0];
+    if (c == 'i' && strcmp(name, "ident") == 0) {
         ypush_int(tao_get_shared_object_ident(obj));
-    } else if (strcmp(name, "type") == 0) {
+        return;
+    } else if (c == 't' && strcmp(name, "type") == 0) {
         ypush_int(tao_get_shared_object_type(obj));
-    } else if (strcmp(name, "size") == 0) {
+        return;
+    } else if (c == 's' && strcmp(name, "size") == 0) {
         ypush_long(tao_get_shared_object_size(obj));
+        return;
     } else {
-        y_error("bad shared object member");
+        int type = tao_get_shared_object_type(obj);
+        if (type == TAO_SHARED_ARRAY) {
+            tao_shared_array_t* arr = (tao_shared_array_t*)obj;
+            if (c == 'c' && strcmp(name, "counter") == 0) {
+                ypush_long(tao_get_shared_array_counter(arr));
+                return;
+            } else if (c == 'e' && strcmp(name, "eltype") == 0) {
+                ypush_long(tao_get_shared_array_eltype(arr));
+                return;
+            } else if (c == 'd' && strcmp(name, "data") == 0) {
+                push_array_data(arr);
+                return;
+            } else if (c == 'd' && strcmp(name, "dims") == 0) {
+                long dimsofdims[2];
+                long* dims;
+                int d, ndims;
+                ndims = tao_get_shared_array_ndims(arr);
+                if (ndims < 0) {
+                    y_error("invalid number of dimensions");
+                }
+                dimsofdims[0] = 1;
+                dimsofdims[1] = ndims + 1;
+                dims = ypush_l(dimsofdims);
+                dims[0] = ndims;
+                for (d = 1; d <= ndims; ++d) {
+                    dims[d] = tao_get_shared_array_size(arr, d);
+                }
+                return;
+            } else if (c == 'n' && strcmp(name, "ndims") == 0) {
+                ypush_long(tao_get_shared_array_ndims(arr));
+                return;
+            } else if (c == 't' && strcmp(name, "timestamp") == 0) {
+                push_array_timestamp(arr);
+                return;
+            }
+        } else if (type == TAO_SHARED_CAMERA) {
+            tao_shared_camera_t* cam = (tao_shared_camera_t*)obj;
+            if (c == 'b' && strcmp(name, "bias") == 0) {
+                ypush_double(tao_get_shared_camera_bias(cam));
+                return;
+            } else if (c == 'd' && strcmp(name, "depth") == 0) {
+                ypush_long(tao_get_shared_camera_depth(cam));
+                return;
+            } else if (c == 'e' && strcmp(name, "eltype") == 0) {
+                ypush_long(tao_get_shared_camera_pixel_type(cam));
+                return;
+            } else if (c == 'e' && strcmp(name, "exposure") == 0) {
+                ypush_double(tao_get_shared_camera_exposure(cam));
+                return;
+            } else if (c == 'f' && strcmp(name, "fullheight") == 0) {
+                ypush_long(tao_get_shared_camera_fullheight(cam));
+                return;
+            } else if (c == 'f' && strcmp(name, "fullwidth") == 0) {
+                ypush_long(tao_get_shared_camera_fullwidth(cam));
+                return;
+            } else if (c == 'g' && strcmp(name, "gain") == 0) {
+                ypush_double(tao_get_shared_camera_gain(cam));
+                return;
+            } else if (c == 'g' && strcmp(name, "gamma") == 0) {
+                ypush_double(tao_get_shared_camera_gamma(cam));
+                return;
+            } else if (c == 'h' && strcmp(name, "height") == 0) {
+                ypush_long(tao_get_shared_camera_height(cam));
+                return;
+            } else if (c == 'r' && strcmp(name, "roi") == 0) {
+                long dims[2];
+                long* roi;
+                dims[0] = 1;
+                dims[1] = 4;
+                roi = ypush_l(dims);
+                roi[0] = tao_get_shared_camera_xoff(cam);
+                roi[1] = tao_get_shared_camera_yoff(cam);
+                roi[2] = tao_get_shared_camera_width(cam);
+                roi[3] = tao_get_shared_camera_height(cam);
+                return;
+            } else if (c == 'r' && strcmp(name, "rate") == 0) {
+                ypush_double(tao_get_shared_camera_rate(cam));
+                return;
+            } else if (c == 's' && strcmp(name, "state") == 0) {
+                ypush_int(tao_get_shared_camera_state(cam));
+                return;
+            } else if (c == 'w' && strcmp(name, "width") == 0) {
+                ypush_long(tao_get_shared_camera_width(cam));
+                return;
+            } else if (c == 'x' && strcmp(name, "xoff") == 0) {
+                ypush_long(tao_get_shared_camera_xoff(cam));
+                return;
+            } else if (c == 'y' && strcmp(name, "yoff") == 0) {
+                ypush_long(tao_get_shared_camera_yoff(cam));
+                return;
+            }
+        }
     }
+    y_error("bad member");
 }
 
 static y_userobj_t shared_object_type = {
@@ -174,12 +359,11 @@ static tao_shared_object_t* get_shared_object(int iarg)
     return *(tao_shared_object_t**)yget_obj(iarg, &shared_object_type);
 }
 
-#if 0
 static tao_shared_array_t* get_shared_array(int iarg)
 {
     tao_shared_object_t* obj = get_shared_object(iarg);
     if (tao_get_shared_object_type(obj) != TAO_SHARED_ARRAY) {
-        y_error("invalid TAO object type");
+        y_error("TAO shared object is not an array");
     }
     return (tao_shared_array_t*)obj;
 }
@@ -187,12 +371,11 @@ static tao_shared_array_t* get_shared_array(int iarg)
 static tao_shared_camera_t* get_shared_camera(int iarg)
 {
     tao_shared_object_t* obj = get_shared_object(iarg);
-    if (tao_get_shared_object_type(obj) != TAO_SHARED_ARRAY) {
-        y_error("invalid TAO object type");
+    if (tao_get_shared_object_type(obj) != TAO_SHARED_CAMERA) {
+        y_error("TAO shared object is not a camera");
     }
     return (tao_shared_camera_t*)obj;
 }
-#endif
 
 void
 Y_tao_attach_shared_object(int argc)
@@ -213,6 +396,48 @@ Y_tao_attach_shared_object(int argc)
         report_errors(&errs);
     }
     *(void**)ypush_obj(&shared_object_type, sizeof(void*)) = obj;
+}
+
+void
+Y_tao_attach_shared_array(int argc)
+{
+    tao_shared_array_t* arr;
+    tao_error_t* errs = TAO_NO_ERRORS;
+    int ident, type;
+    if (argc != 1) {
+        y_error("expecting exactly 1 argument");
+    }
+    ident = ygets_l(argc - 1);
+    type = (argc >= 2 ? ygets_l(argc - 2) : TAO_SHARED_ANY);
+    if (0x00 <= type && type <= 0xff) {
+        type |= TAO_SHARED_MAGIC;
+    }
+    arr = tao_attach_shared_array(&errs, ident);
+    if (errs != TAO_NO_ERRORS) {
+        report_errors(&errs);
+    }
+    *(void**)ypush_obj(&shared_object_type, sizeof(void*)) = arr;
+}
+
+void
+Y_tao_attach_shared_camera(int argc)
+{
+    tao_shared_camera_t* cam;
+    tao_error_t* errs = TAO_NO_ERRORS;
+    int ident, type;
+    if (argc != 1) {
+        y_error("expecting exactly 1 argument");
+    }
+    ident = ygets_l(argc - 1);
+    type = (argc >= 2 ? ygets_l(argc - 2) : TAO_SHARED_ANY);
+    if (0x00 <= type && type <= 0xff) {
+        type |= TAO_SHARED_MAGIC;
+    }
+    cam = tao_attach_shared_camera(&errs, ident);
+    if (errs != TAO_NO_ERRORS) {
+        report_errors(&errs);
+    }
+    *(void**)ypush_obj(&shared_object_type, sizeof(void*)) = cam;
 }
 
 void
@@ -241,6 +466,56 @@ Y_tao_create_shared_object(int argc)
     } else {
         tao_discard_errors(&errs);
         *(void**)ypush_obj(&shared_object_type, sizeof(void*)) = obj;
+    }
+}
+
+void
+Y_tao_wait_image(int argc)
+{
+    int sem = 5; /* index of semaphore to use */
+    tao_shared_camera_t* cam;
+    tao_error_t* errs = TAO_NO_ERRORS;
+    int ans, ident, iarg;
+
+    /* Check number of arguments. */
+    if (argc < 1 || argc > 2) {
+        y_error("expecting 1 or 2 arguments");
+    }
+
+    /* Parse first argument. */
+    iarg = argc - 1;
+    cam = get_shared_camera(iarg);
+
+    /* Parse second argument and wait for next image. */
+    iarg = argc - 2;
+    if (iarg < 0 || yarg_nil(iarg)) {
+        if (tao_wait_image(&errs, cam, sem) == 0) {
+            ans = 1;
+        } else {
+            ans = -1;
+        }
+    } else {
+        double secs = ygets_d(iarg);
+        ans = tao_timed_wait_image(&errs, cam, sem, secs);
+    }
+    if (errs != TAO_NO_ERRORS) {
+        report_errors(&errs);
+    }
+
+    /* Attach new image if any. */
+    if (ans == 1) {
+        ident = tao_get_last_image_ident(cam);
+    } else {
+        ident = -1;
+    }
+    if (ident == -1) {
+        ypush_nil();
+    } else {
+        tao_shared_array_t* arr = tao_attach_shared_array(&errs, ident);
+        if (errs != TAO_NO_ERRORS) {
+            report_errors(&errs);
+        }
+        *(void**)ypush_obj(&shared_object_type, sizeof(void*)) = arr;
     }
 }
 
@@ -327,20 +602,90 @@ Y_tao_try_lock(int argc)
 }
 
 void
-Y_tao_test(int argc)
+Y_tao_get_data(int argc)
 {
-    push_string("hello!");
+    if (argc != 1) {
+        y_error("expecting exactly one argument");
+    }
+    push_array_data(get_shared_array(0));
 }
 
-static void
-push_string(const char* str)
+/*
+ * Calling a built-in function is slightly faster than extracting an object
+ * member:
+ *
+ *  `tao_get_counter(arr)` -----> 32 ns     `arr.counter` -----> 56 ns
+ *  `tao_get_timestamp(arr)` ---> 76 ns     `arr.timestamp` ---> 106 ns
+ *
+ * So it seems that there is an overhead of ~ 30 ns when extracting an object
+ * member.
+ */
+void
+Y_tao_get_counter(int argc)
 {
-  ypush_q(NULL)[0] = p_strcpy(str);
+    if (argc != 1) {
+        y_error("expecting exactly one argument");
+    }
+    ypush_long(tao_get_shared_array_counter(get_shared_array(0)));
+}
+
+void
+Y_tao_get_timestamp(int argc)
+{
+    if (argc != 1) {
+        y_error("expecting exactly one argument");
+    }
+    push_array_timestamp(get_shared_array(0));
+}
+
+static void initialize_type(size_t size, int ytype, int floatingpoint)
+{
+    if (floatingpoint) {
+        if (size == 4) {
+            tao_to_yorick_types[TAO_FLOAT32 - TAO_INT8] = ytype;
+            yorick_to_tao_types[ytype - Y_CHAR] = TAO_FLOAT32;
+        } else if (size == 8) {
+            tao_to_yorick_types[TAO_FLOAT64 - TAO_INT8] = ytype;
+            yorick_to_tao_types[ytype - Y_CHAR] = TAO_FLOAT64;
+        }
+    } else {
+        if (size == 1) {
+            tao_to_yorick_types[TAO_INT8 - TAO_INT8] = ytype;
+            tao_to_yorick_types[TAO_UINT8 - TAO_INT8] = ytype;
+            yorick_to_tao_types[ytype - Y_CHAR] = TAO_UINT8;
+        } else if (size == 2) {
+            tao_to_yorick_types[TAO_INT16 - TAO_INT8] = ytype;
+            tao_to_yorick_types[TAO_UINT16 - TAO_INT8] = ytype;
+            yorick_to_tao_types[ytype - Y_CHAR] = TAO_INT16;
+        } else if (size == 4) {
+            tao_to_yorick_types[TAO_INT32 - TAO_INT8] = ytype;
+            tao_to_yorick_types[TAO_UINT32 - TAO_INT8] = ytype;
+            yorick_to_tao_types[ytype - Y_CHAR] = TAO_UINT32;
+        } else if (size == 8) {
+            tao_to_yorick_types[TAO_INT64 - TAO_INT8] = ytype;
+            tao_to_yorick_types[TAO_UINT64 - TAO_INT8] = ytype;
+            yorick_to_tao_types[ytype - Y_CHAR] = TAO_UINT64;
+        }
+    }
 }
 
 void
 Y__tao_init(int argc)
 {
+    int i;
+    for (i = TAO_INT8; i <= TAO_FLOAT64; ++i) {
+        tao_to_yorick_types[i - TAO_INT8] = -1;
+    }
+    for (i = Y_CHAR; i <= Y_DOUBLE; ++i) {
+        yorick_to_tao_types[i - Y_CHAR] = -1;
+    }
+    initialize_type(sizeof(char),   Y_CHAR,   0);
+    initialize_type(sizeof(short),  Y_SHORT,  0);
+    initialize_type(sizeof(int),    Y_INT,    0);
+    initialize_type(sizeof(long),   Y_LONG,   0);
+    initialize_type(sizeof(float),  Y_FLOAT,  1);
+    initialize_type(sizeof(double), Y_DOUBLE, 1);
+
 #define DEF_LONG_CONST(id)                      \
     do {                                        \
         ypush_long(id);                         \
@@ -351,6 +696,16 @@ Y__tao_init(int argc)
     DEF_LONG_CONST(TAO_SHARED_OBJECT);
     DEF_LONG_CONST(TAO_SHARED_ARRAY);
     DEF_LONG_CONST(TAO_SHARED_CAMERA);
+    DEF_LONG_CONST(TAO_INT8);
+    DEF_LONG_CONST(TAO_UINT8);
+    DEF_LONG_CONST(TAO_INT16);
+    DEF_LONG_CONST(TAO_UINT16);
+    DEF_LONG_CONST(TAO_INT32);
+    DEF_LONG_CONST(TAO_UINT32);
+    DEF_LONG_CONST(TAO_INT64);
+    DEF_LONG_CONST(TAO_UINT64);
+    DEF_LONG_CONST(TAO_FLOAT32);
+    DEF_LONG_CONST(TAO_FLOAT64);
 #undef DEF_LONG_CONST
     ypush_nil();
 }
