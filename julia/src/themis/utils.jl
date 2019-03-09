e9aa799527057728efa8a19cf3e8d23cd2620e92
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-03-09 12:24:54 +0000

    Rename method makeshape() as pupilshape(), add makegrid() method

--- julia/src/themis/utils.jl
+++ julia/src/themis/utils.jl
@@ -11,7 +11,17 @@
 # Copyright (C) 2018-2019, Éric Thiébaut and Michel Tallon.
 #
 
-function makeshape(rows::AbstractString...)
+"""
+
+```julia
+pupilshape(args...) -> msk
+```
+
+yields a 2D mask (a Julia matrix of boolean elements) defining the
+valid/active positions in a pupil.
+
+"""
+function pupilshape(rows::AbstractString...)
     nrows = length(rows)
     ncols = length(rows[1])
     A = Array{Bool,2}(undef, ncols, nrows)
@@ -25,7 +35,7 @@ function makeshape(rows::AbstractString...)
     return A
 end
 
-makeshape(A::AbstractMatrix{Bool}) = A
+pupilshape(A::AbstractMatrix{Bool}) = A
 
 """
 
@@ -35,7 +45,7 @@ adaptive optics system.  Without the central obscuration, use
 [`Themis.WFS_FULL_SHAPE`](@ref).
 
 """
-const WFS_SHAPE = makeshape(
+const WFS_SHAPE = pupilshape(
     "  xxxxxx  ",
     " xxxxxxxx ",
     "xxxxxxxxxx",
@@ -55,7 +65,7 @@ adaptive optics system.  With the central obscuration, use
 [`Themis.WFS_SHAPE`](@ref).
 
 """
-const WFS_FULL_SHAPE = makeshape(
+const WFS_FULL_SHAPE = pupilshape(
     "  xxxxxx  ",
     " xxxxxxxx ",
     "xxxxxxxxxx",
@@ -73,7 +83,7 @@ const WFS_FULL_SHAPE = makeshape(
 actuators of the deformable mirror of Themis adaptive optics system.
 
 """
-const DM_SHAPE = makeshape(
+const DM_SHAPE = pupilshape(
     "   xxxxx   ",
     "  xxxxxxx  ",
     " xxxxxxxxx ",
@@ -85,6 +95,32 @@ const DM_SHAPE = makeshape(
     " xxxxxxxxx ",
     "  xxxxxxx  ",
     "   xxxxx   ");
+
+function makegrid(pix1::NTuple{2,Real}, grd1::NTuple{2,Real},
+                  pix2::NTuple{2,Real}, grd2::NTuple{2,Real};
+                  shape::AbstractMatrix{Bool} = WFS_SHAPE)
+    stp = (pix2 .- pix1)./(grd2 .- grd1)
+    @printf("grid step: (%.3f,%.3f) pixels per grid node\n", stp...)
+    avgstp = hypot(stp...)/sqrt(2)
+    @printf("average grid step: %.3f pixels per grid node\n", avgstp)
+    nsubs = count(shape)
+    @printf("number of active sub-pupils: %d\n", nsubs)
+    grid = Matrix{Cdouble}(undef, 2, nsubs)
+    grd0 = (grd1 .+ grd2)./2
+    pix0 = (pix1 .+ pix2)./2
+    width, height = size(shape)
+    k = 0
+    for gy in 1:height
+        for gx in 1:width
+            if shape[gx,gy]
+                k += 1
+                grid[:,k] .= ((gx, gy) .- grd0).*avgstp .+ pix0
+            end
+        end
+    end
+    @assert k == nsubs
+    return grid
+end
 
 """
 
